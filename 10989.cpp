03d4afded9e17454b7b0cd6baef369a2e170df09
NamSangWon <nsw3265@naver.com>
2025-04-02 16:46:45 +0900

    Check Memory Limit (Using Counting Sort)

--- 10989.cpp
+++ 10989.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main()
 {
     // [메모리 초과]를 고려해야하는 문제!!
+    // 숫자 입력 갯수 (N) : 10,000,000개 => 자료형 int
+    // 입력 받는 숫자 : 10,000 이하의 자연수 => 자료형 short
     int nums[10001] = {0,};
 
     int N;
     cin >> N;
 
-    // 계수 정렬 사용
+    // Counting Sort 사용
     for (int i = 0; i < N; i++)
     {
         short num;
